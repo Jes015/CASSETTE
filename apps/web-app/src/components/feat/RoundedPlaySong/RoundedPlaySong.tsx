17caaa8728c281cc8136142d4ee3c42112d87da6
Jes015 <jes.18.sin.15@gmail.com>
2024-05-18 16:27:56 -0500

    feat(web-app): add Image component (#18)
    close #18

--- apps/web-app/src/components/feat/RoundedPlaySong/RoundedPlaySong.tsx
+++ apps/web-app/src/components/feat/RoundedPlaySong/RoundedPlaySong.tsx
@@ -1,10 +1,10 @@
 'use client'
+import { Image } from "@/components/ui/Image/Image"
 import { BaseComponentProps } from "@/models/component.model"
 import { songPlayerService } from "@/services/client/song-player.client-service"
 import { IconPlayerPlayFilled } from "@tabler/icons-react"
 import clsx from "clsx"
-import { FC } from "react"
-import styles from './roundedPlaySong.module.css'
+import { FC, MouseEvent } from "react"
 
 interface RoundedPlaySongProps extends BaseComponentProps {
     art?: string
@@ -13,7 +13,7 @@ interface RoundedPlaySongProps extends BaseComponentProps {
 
 export const RoundedPlaySong: FC<RoundedPlaySongProps> = ({ art = 'https://i.ytimg.com/vi/ssdN7ZfavHs/maxresdefault.jpg', size = 'small', className, ...props }) => {
 
-    const handleOnClickToPlayArt = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const handleOnClickToPlayArt = (e: MouseEvent<HTMLDivElement, MouseEvent>) => {
         e.preventDefault()
         e.stopPropagation()
 
@@ -25,42 +25,30 @@ export const RoundedPlaySong: FC<RoundedPlaySongProps> = ({ art = 'https://i.yti
             onClick={handleOnClickToPlayArt}
             className={
                 clsx(
-                    "relative",
+                    "relative overflow-hidden",
                     size === 'big' && 'w-40 h-40',
                     size === 'small' && 'w-14 h-14'
                 )
             }
             {...props}
         >
-            <img
-                src="https://i.ytimg.com/vi/ssdN7ZfavHs/maxresdefault.jpg" alt=""
+            <Image
+                loading="eager"
+                width={size === 'big' ? 160 : 90}
+                height={size === 'big' ? 160 : 90}
+                src="/tainy.webp"
+                alt=""
                 className={
                     clsx(
-                        "rounded-full w-14 h-14 aspect-square object-cover",
-                        size === 'big' && 'w-40 h-40',
-                        size === 'small' && 'w-14 h-14'
+                        "!rounded-full w-14 h-14 aspect-square object-cover",
+                        size === 'big' && 'w-40 !h-40',
+                        size === 'small' && 'w-14 !h-14'
                     )
                 }
             />
             <div
                 className="flex justify-center items-center w-full h-full absolute top-0 left-0"
             >
-                <div
-                    className={
-                        clsx(
-                            "absolute z-20",
-                            styles.circularProgressBar,
-                            size === 'small' && 'circularProgressBar--small',
-                            size === 'big' && 'circularProgressBar--big',
-                        )
-                    }
-                    style={{
-                        // @ts-expect-error The property --bar-width is for the width of the loader bard
-                        '--bar-width': size === 'big' ? '20px' : '10px',
-                        '--progress': '84%'
-                    }}
-                >
-                </div>
                 <button
                     onClick={handleOnClickToPlayArt}
                     className="z-40 flex items-center w-full h-full justify-center rounded-full bg-bg-primary/80 p-2 opacity-40 hover:opacity-85 transition-all"
