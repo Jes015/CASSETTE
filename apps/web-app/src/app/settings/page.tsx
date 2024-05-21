29f48a828bc0a4687eaa3d2f2eb0a2ddab89da0e
Jes015 <jes.18.sin.15@gmail.com>
2024-05-21 17:36:40 -0500

    feat(web-app): add Account section (#35)
    resolves #35

--- apps/web-app/src/app/settings/page.tsx
+++ apps/web-app/src/app/settings/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/Button/Button"
 import { Paragraph } from "@/components/ui/Paragraph/Paragraph"
 import { Section } from "@/components/ui/Section/Section"
 import { Title } from "@/components/ui/Title/Title"
@@ -17,9 +18,9 @@ const SettingsHomePage: PageType = (props) => {
                 description="Basic settings of the UI"
             />
             <Section.Content
-                className="w-full flex gap-[0.2rem] p-1"
+                className="w-full flex flex-col items-start px-2 gap-2 pt-1 pb-2"
             >
-                <section className="px-2">
+                <section>
                     <header>
                         <Title>General</Title>
                         <Paragraph as="quaternary">Color, design and theme settings</Paragraph>
@@ -27,6 +28,16 @@ const SettingsHomePage: PageType = (props) => {
                     <div className="flex flex-col gap-2 pt-2">
                         <SettingsThemeInput />
                         <SettingsLanguageInput />
+                    </div>
+                </section>
+                <section>
+                    <header>
+                        <Title>Account</Title>
+                        <Paragraph as="quaternary">Delete and disable account</Paragraph>
+                    </header>
+                    <div className="flex flex-col gap-1 pt-2">
+                        <Button className="!bg-red-400/10 hover:!bg-red-400/20">Delete account</Button>
+                        <Button className="!bg-yellow-400/10 hover:!bg-yellow-400/20">Disable account</Button>
                     </div>
                 </section>
             </Section.Content>
