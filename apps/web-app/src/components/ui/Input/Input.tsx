2fc85b381cee9aa7d2b1313b6cdc2413de36c27b
Jes015 <jes.18.sin.15@gmail.com>
2024-05-21 18:33:36 -0500

    feat(web-app): add Settings page (#35)
    close #35

--- apps/web-app/src/components/ui/Input/Input.tsx
+++ apps/web-app/src/components/ui/Input/Input.tsx
@@ -1,7 +1,14 @@
 import { BaseComponentType } from "@/models/component.model"
+import clsx from "clsx"
 
-export const Input: BaseComponentType = (props) => {
-   return (
-       <input type="text" />
-   )
+export const Input: BaseComponentType = ({ className, ...props }) => {
+    return (
+        <input
+            type="text"
+            className={
+                clsx('', className)
+            }
+            {...props}
+        />
+    )
 }
