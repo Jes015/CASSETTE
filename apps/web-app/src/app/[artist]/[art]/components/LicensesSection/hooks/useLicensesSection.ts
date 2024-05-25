2ba6fde2fa7ee235d8eecb4672f0fb60aee99986
Jes015 <jes.18.sin.15@gmail.com>
2024-05-25 14:27:34 -0500

    feat(web-app): add licenses logic (#41)
    close #41

--- apps/web-app/src/app/[artist]/[art]/components/LicensesSection/hooks/useLicensesSection.ts
+++ apps/web-app/src/app/[artist]/[art]/components/LicensesSection/hooks/useLicensesSection.ts
@@ -1,12 +1,16 @@
-import { LicenseType, licensesNames } from "@/models/licenses.model"
+import { License, licensesData } from "@/models/licenses.model"
 import { useState } from "react"
 
 export const useLicensesSection = () => {
-    const [currentLicense, setCurrentLicense] = useState<LicenseType>(licensesNames.basic)
+    const [currentLicense, setCurrentLicense] = useState<License>(licensesData[0])
 
-    const changeLicense = (newLicense: LicenseType) => {
-        setCurrentLicense(newLicense)
+    const changeLicense = (newLicenseName: string) => {
+        const license = licensesData.find((license) => license.name === newLicenseName)
+
+        if (license == null) return
+
+        setCurrentLicense(license)
     }
 
-    return { changeLicense, currentLicense }
+    return { changeLicense, currentLicense, licensesData }
 }
--- apps/web-app/src/models/licenses.model.ts
+++ apps/web-app/src/models/licenses.model.ts
@@ -1,8 +1,80 @@
-export const licensesNames = {
-    basic: 'Basic',
-    pro: 'Pro',
-    steams: 'Steams',
-    exclusive: 'Exclusive'
-} as const
+export interface LicenseInclude {
+    name: string
+    type: 'radio' | 'video' | 'listening' | 'steams'
+}
 
-export type LicenseType = typeof licensesNames[keyof typeof licensesNames]
+export type LicenseIncludeArray = LicenseInclude[]
+
+export interface License {
+    name: string
+    includes: LicenseIncludeArray
+}
+
+export type LicenseArray = License[]
+
+export const licensesData: LicenseArray = [
+    {
+        name: 'Basic',
+        includes: [
+            {
+                name: '4000 Radio listenings',
+                type: 'radio'
+            },
+            {
+                name: 'Up to 4000 Listenings',
+                type: 'listening'
+            }
+        ]
+    },
+    {
+        name: 'Pro ',
+        includes: [
+            {
+                name: '80000 Radio listenings',
+                type: 'radio'
+            },
+            {
+                name: 'Up to 80000 Listenings',
+                type: 'listening'
+            }
+        ]
+    },
+    {
+        name: 'Pro + Steams',
+        includes: [
+            {
+                name: '90000 Radio listenings',
+                type: 'radio'
+            },
+            {
+                name: 'Up to 90000 Listenings',
+                type: 'listening'
+            },
+            {
+                name: 'Steams',
+                type: 'steams'
+            }
+        ]
+    },
+    {
+        name: 'Unlimited',
+        includes: [
+            {
+                name: 'Unlimited Radio listenings',
+                type: 'radio'
+            },
+            {
+                name: 'Unlimited Listenings',
+                type: 'listening'
+            },
+            {
+                name: 'Unlimited videos',
+                type: 'video'
+            },
+            {
+                name: 'Steams',
+                type: 'steams'
+            }
+        ]
+    }
+]
