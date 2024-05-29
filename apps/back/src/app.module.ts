59cd190e7dfe87fe4adcd139da3c6c9ca19b2522
Jes015 <jes.18.sin.15@gmail.com>
2024-05-29 14:41:13 -0500

    feat(back): add envs validations (#60)
    close #60

--- apps/back/src/app.module.ts
+++ apps/back/src/app.module.ts
@@ -3,11 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { envValidation } from './config/env.validation';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      validationSchema: envValidation,
+    }),
     UserModule,
     TypeOrmModule.forRoot({
       ssl: process.env.DB_SSL === 'true',
